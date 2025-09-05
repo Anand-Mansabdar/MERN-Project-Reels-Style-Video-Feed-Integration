b6eed8132c613df55062e24af74ec78fe90596bb
Anand-Mansabdar <dummystudy040105@gmail.com>
2025-09-06 00:11:30 +0530

    scaffold: Implemented Home and Create food frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require('cors');
 
 const authRoutes = require("./routes/auth.routes");
 const foodRoutes = require("./routes/food.routes");
 
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}))
 app.use(express.json());
 app.use(cookieParser());
 
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserRegister from "../pages/auth/UserRegister";
+import UserLogin from "../pages/auth/UserLogin";
+import FoodPartnerRegister from "../pages/auth/FoodPartnerRegister";
+import FoodPartnerLogin from "../pages/auth/FoodPartnerLogin";
+import ChooseRegister from '../pages/auth/ChooseRegister';
+import Home from "../pages/general/Home";
+import CreateFood from "../pages/food-partner/CreateFood";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/register" element={<ChooseRegister />} />
         <Route path="/user/register" element={<UserRegister />} />
         <Route path="/user/login" element={<UserLogin />} />
-        <Route path="/food-partner/register" element={<FoodPartnerRegister />} />
+        <Route
+          path="/food-partner/register"
+          element={<FoodPartnerRegister />}
+        />
         <Route path="/food-partner/login" element={<FoodPartnerLogin />} />
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/create-food" element={<CreateFood />} ></Route>
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
